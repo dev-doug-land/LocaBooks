59c6dcfd85104ad2383b6ba4549770bbcdb5016c
Douglas Stark <douglassan1@hotmail.com>
2025-07-26 16:26:22 -0300

    Feat: controller de  locação criado

---
+++ LocaBooks/Controllers/LocacaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocaBooks.Controllers
+{
+    public class LocacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
